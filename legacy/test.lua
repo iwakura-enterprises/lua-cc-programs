15e425cda190f69fad616f016a3f3e6d72a9e5fe
lilmayu <uwulilmayu@gmail.com>
2025-04-13 13:51:44 +0200

    testing lua

--- legacy/test.lua
+++ legacy/test.lua
@@ -4,11 +4,13 @@ local ae = peripheral.wrap("ae2cc_adapter_0")
 -- { { type = string, id = string, displayName = string, amount = number }... }
 local availableObjects = ae.getAvailableObjects()
 
+local all_items_count = 0
+
 for i = 1, #availableObjects do
   local obj = availableObjects[i]
-  print("Type: " .. obj.type)
-  print("ID: " .. obj.id)
-  print("Display Name: " .. obj.displayName)
-  print("Amount: " .. obj.amount)
-  sleep(1)
+  all_items_count = all_items_count + obj.amount
+  print("Amount: " .. all_items_count)
 end
+
+print("")
+print("Final all items count: " .. all_items_count)
